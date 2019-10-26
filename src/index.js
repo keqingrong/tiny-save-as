31351ba04e1e0ba6ea34f7be039dfb736d7b52a7
Qingrong Ke <keqingrong1992@gmail.com>
2019-10-26 16:23:21 +0800

    Add code comments

--- src/index.js
+++ src/index.js
@@ -18,20 +18,23 @@ function saveAs(blob, filename) {
     link.href = URL.createObjectURL(blob);
     link.download = filename;
     link.addEventListener('click', function () {
-      // 结束后释放 URL 对象，但如果立即调用 revokeObjectURL 会导致网络错误
-      // 如 Chrome: "Failed - Network error"
+      // 结束后释放 URL 对象，但如果立即调用 revokeObjectURL 会导致网络错误。
+      // 如 Chrome 会提示 "Failed - Network error"，此处放在回调函数中异步处理。
       requestAnimationFrame(function () {
         URL.revokeObjectURL(link.href);
         link.remove();
       })
     }, false);
 
+    // 没有必要调用 document.body.appendChild() 将 link 元素真的添加到页面中。
+    // 因为没有添加到页面中，此处调用 link.click() 对 Firefox 来说不会触发下载，
+    // 使用 link.dispatchEvent() 代替。
     link.dispatchEvent(new MouseEvent('click'));
     return true;
   }
 
-  // IE9/Safari for iOS/Chrome for iOS
-  console.warn('该浏览器不支持文件保存'); // eslint-disable-line no-console
+  // 不支持 IE9 等低版本浏览器和 Safari for iOS、Chrome for iOS 等 iOS 平台浏览器
+  console.warn('[tiny-save-as] 该浏览器不支持文件保存'); // eslint-disable-line no-console
   return false;
 }
 
